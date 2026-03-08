fc74e5db1c1a9d69faf722614acb865e3469ba54
William <wi1123.syd@gmail.com>
2026-03-08 12:07:14 +0700

    worker_volatility: constructor

--- worker_volatility.h
+++ worker_volatility.h
@@ -1,12 +1,35 @@
 #ifndef WORKER_H
 #define WORKER_H
 
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
 #include <queue>
+#include <string>
+#include <unordered_map>
 #include "trade_packet.h"
+#include "welford_volatility.h"
 
 class WorkerVolatility {
     private:
-        std::queue<TradePacket> local_queue;
+        const std::atomic_bool& m_running;
+        std::queue<TradePacket> m_channel;
+        std::unordered_map<std::string, WelfordVolatility> welfords;
+
+    public:
+        std::queue<TradePacket> channel;
+        std::mutex mux;
+        std::condition_variable cv;
+
+        WorkerVolatility(const std::atomic_bool& running)
+            : m_running {running}
+            , m_channel {}
+            , welfords {}
+            , channel {}
+            , mux {}
+            , cv {}
+        {
+        }
 };
 
 #endif
